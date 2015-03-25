4017fa62ac2c732bc19826bdf726297f275fde1c
unknown <tomk@DATPCDI064.digiterre.com>
2015-03-25 16:15:55 +0000

    Creating a chocopacks file to build up vagrant box with required tools

---
+++ scripts/chocopacks_developer.bat
@@ -0,0 +1,7 @@
+:: Ensure C:\Chocolatey\bin is on the path
+set /p PATH=<C:\Windows\Temp\PATH
+
+:: Install all the things; for example:
+cmd /c choco install git -y -params '/GitAndUnixToolsOnPath'
+cmd /c choco install mssqlserver2014express -v 1.0.5
+cmd /c choco install visualstudio2013professional -y -v 12.0.21005.20141031 -InstallArguments "/Features:'Blend' 'SQL' 'WebTools' 'Win8SDK'"
