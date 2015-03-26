55238367f7f7b591e511bc6ff3c838ea055efab1
unknown <tomk@DATPCDI064.digiterre.com>
2015-03-26 10:25:22 +0000

    Update the json file to include step to install Chocolatey and added some additional development tools to chocopacks_developer.bat

--- scripts/chocopacks_developer.bat
+++ scripts/chocopacks_developer.bat
@@ -4,4 +4,11 @@ set /p PATH=<C:\Windows\Temp\PATH
 :: Install all the things; for example:
 cmd /c choco install git -y -params '/GitAndUnixToolsOnPath'
 cmd /c choco install mssqlserver2014express -v 1.0.5
+
+:: .NET Tools
 cmd /c choco install visualstudio2013professional -y -v 12.0.21005.20141031 -InstallArguments "/Features:'Blend' 'SQL' 'WebTools' 'Win8SDK'"
+cmd /c choco install SpecFlow -y
+cmd /c choco install NUnit -y
+
+:: Web tools
+cmd /c choco install google-chrome-x64 -y
